1f3bb0fe4d8c8d04431e5ff99f7b94bff4747471
Mike Liu <mike.liu@nimbisservices.com>
2016-04-17 17:05:06 -0400

    Complete 'Implement Navigation' lesson.
    Key Objectives:
        * Embed view in a navigation controller
        * Create an unwind segue
        * 'Add' button segue to meal view
        * Dismiss a view without changes

--- xcode/Food Tracker/Food Tracker/MealViewController.swift
+++ xcode/Food Tracker/Food Tracker/MealViewController.swift
@@ -15,10 +15,22 @@ class MealViewController: UIViewController, UITextFieldDelegate,
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+
+    /*
+     This value is either passed by `MealTableViewController` in `prepareForSegue(_:sender:)`
+     or constructed as part of adding a new meal.
+     */
+    var meal: Meal?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Handle the text field's user input through delegate callbacks.
         nameTextField.delegate = self
+
+        // Enable the Save button only if the text field has a valid Meal Name.
+        checkValidMealName()
     }
 
     // MARK: UITextFieldDelegate
@@ -28,7 +40,20 @@ class MealViewController: UIViewController, UITextFieldDelegate,
         return true
     }
 
+    func textFieldDidBeginEditing(textField: UITextField) {
+        // Disable save button when editing.
+        saveButton.enabled = false
+    }
+
+    func checkValidMealName() {
+        // Disable the save button if the text field is empty.
+        let text = nameTextField.text ?? ""
+        saveButton.enabled = !text.isEmpty
+    }
+
     func textFieldDidEndEditing(textField: UITextField) {
+        checkValidMealName()
+        navigationItem.title = textField.text
     }
 
     // MARK: UIImagePickerControllerDelegate
@@ -42,6 +67,24 @@ class MealViewController: UIViewController, UITextFieldDelegate,
         let selectedImage = info[UIImagePickerControllerOriginalImage] as!
             UIImage
         photoImageView.image = selectedImage
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    // MARK: Navigation
+    // This method lets you configure a view controller before it's presented.
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if saveButton === sender {
+            let name = nameTextField.text ?? ""
+            let photo = photoImageView.image
+            let rating = ratingControl.rating
+
+            // Set the meal to be passed ot the MealTableViewController after
+            // the unwind segue.
+            meal = Meal(name: name, photo: photo, rating: rating)
+        }
+    }
+
+    @IBAction func cancel(sender: UIBarButtonItem) {
         dismissViewControllerAnimated(true, completion: nil)
     }
 
